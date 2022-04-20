b901319bffb2dae6693d4d066a65da9e24979943
C:\S10222023\programmer\launch   LZY.exe <93893933+WindowsCommandPrompt@users.noreply.github.com>
2022-04-20 18:52:35 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -17,7 +17,18 @@ public class Question4
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
      
-    Scanner in = new Scanner(System.in);
-    
+    Scanner scr = new Scanner(System.in);
+		//Prints a right angle triangle based on the number of stars given. The length of the base is 
+		//determined by the value that the user has input. 
+		System.out.print("Please enter the length of the base: ");
+		int len = scr.nextInt(); 
+		do {
+			for (int i = 0; i < len; i++) {
+				System.out.print("*");
+			}
+			System.out.println(); 
+			len--; 
+		} 
+		while (len > 0); 
   }
 }
