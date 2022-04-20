5de99871c9b049be29a6fcbf3778d2949c29b9cf
C:\S10222023\programmer\launch   LZY.exe <93893933+WindowsCommandPrompt@users.noreply.github.com>
2022-04-20 18:51:11 +0800

    Update Question3.java
    added: 
    
    		//Reads the integer entered by the user, multiplies by itself and then prints out the result... 
    		System.out.print("Please enter a number: ");
    		double num = scr.nextDouble(); 
    		double result = Math.pow(num, num); 
    		System.out.println("The result when the number is multiplied by itself is: " + result);

--- Question3.java
+++ Question3.java
@@ -10,7 +10,11 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
-    
+    Scanner scr = new Scanner(System.in);
+    //Reads the integer entered by the user, multiplies by itself and then prints out the result... 
+		System.out.print("Please enter a number: ");
+		double num = scr.nextDouble(); 
+		double result = Math.pow(num, num); 
+		System.out.println("The result when the number is multiplied by itself is: " + result); 
   }
 }
