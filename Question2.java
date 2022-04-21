5d98254122d6d82851b779e2d62aa832f03a99cf
Li Zhe Yun <93893933+WindowsCommandPrompt@users.noreply.github.com>
2022-04-21 19:09:23 +0800

    Question 2 done

--- Question2.java
+++ Question2.java
@@ -9,13 +9,13 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-    Scanner scr = new Scanner(System.in); 
-    //prompt the user for height and weight, calculate the bmi, and then display the bmi back to the user... 
-		System.out.print("Please enter your weight(in kilograms): ");
-		double weight = scr.nextDouble(); 
-		System.out.print("Thanks. Now please enter your height (in metres): ");
-		double height = scr.nextDouble(); 
-		double bmi = weight / Math.pow(height, 2); 
-		System.out.println("Your bmi is: " + bmi); 
+      Scanner scr = new Scanner(System.in); 
+      //prompt the user for height and weight, calculate the bmi, and then display the bmi back to the user... 
+      System.out.print("Please enter your weight(in kilograms): ");
+      double weight = scr.nextDouble(); 
+      System.out.print("Thanks. Now please enter your height (in metres): ");
+      double height = scr.nextDouble(); 
+      double bmi = weight / Math.pow(height, 2); 
+      System.out.println("Your bmi is: " + bmi); 
   }
 }
